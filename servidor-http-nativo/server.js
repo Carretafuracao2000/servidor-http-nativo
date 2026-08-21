3080a34345d5feef799dae92ab5bae18cfa2c51a
Carretafuracao2000 <joao.santos71@estudante.ifms.edu.br>
2026-08-20 22:24:31 -0400

    exercicios da semana passada

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -1,18 +1,58 @@
-const http = require('node:http')
-const PORTA = 3000 
-const server = http.createServer((req, res) =>{
-    console.log(`Requisição recebida! ${req.method} ${req.url}`)
+import http from 'node:http'
+import { URL } from 'node:url'
 
-    res.statusCode = 201
-    res.setHeader('Content-type', 'application/json; charset=utf-8')
+const PORTA = 3000
+const produtos = [
+    {id: 1, nome: "Sabonete"},
+    {id: 2, nome: "Volante Logitech"},
+    {id: 3, nome: "Sabão em Pó"},
+    {id: 4, nome: "Pelucia do Sonic"}
+]
 
-    res.end(JSON.stringify({ status: "ok" }))
-    // Sem ele o servidor não carrega nunca.
+//res de response e req de request
+const server = http.createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+
+    if(req.method == "GET" && req.url == "/"){
+        return res.end(
+            JSON.stringify(JSON.stringify({data: "Página inicial"}))
+        )
+    }
+
+    if(req.method == "GET" && req.url == "/contato"){
+        console.log(`${req.method} ${req.url}`)
+
+        return res.end(
+            JSON.stringify([
+                {data: {"número_telefone": "67-99999-9999", endereco: "Rua Alegria, 99, Centro"}}
+            ])
+        )
+    }
+
+    if(req.method == "GET" && req.url == "/produtos"){
+        console.log(`${req.method} ${req.url}`)
+
+        return res.end(
+            JSON.stringify(produtos)
+        )
+    }
+
+    if(req.method == "GET" && req.url == "/status"){
+        console.log(`${req.method} ${req.url}`)
+
+        return res.end(
+            JSON.stringify({status: "ok"})
+        )
+    }
+
+    else{
+        return res.end(JSON.stringify(`Pagina_inexistente! Error ${res.statusCode = 404}`))
+    }
 })
 
 server.listen(PORTA, () => {
     console.log(`Servidor funcionando na porta ${PORTA}`)
-    console.log(new Date().toISOString())
-   
-    
 })
+
+//o servidor não devolveria uma resposta para o usuario, pois a requisição não seria resolvida assim o serividor iria ficar carregando infinitamente
